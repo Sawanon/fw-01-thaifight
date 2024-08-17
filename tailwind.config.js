2b7e718651c737c1bfe8d5226d4e17e02758fab2
sawanon <sawanon01@hotmail.com>
2024-08-18 01:20:07 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'test': "green"
+      },
+      boxShadow: {
+        'button': '0px 4px',
+        'button-sm': '0px 2px',
+      },
+      fontFamily: {
+        'prompt': 'Prompt'
+      },
+      animation: {
+        'marquee': 'marquee 10s linear infinite'
+      },
+      keyframes: {
+        'marquee': {
+          '0%': {
+            transform: 'translateX(100%)',
+          },
+          '80%': {
+            transform: 'translateX(0%)',
+          },
+          '100%': {
+            transform: 'translateX(-10%)',
+          },
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
